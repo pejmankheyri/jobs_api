5f26a3a0fd987c09e85d4f7302b2147d8ac6b6d3
pejmankheyri <pejmankheyri@gmail.com>
2024-06-09 20:19:06 +0200

    refactor: search controller

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\JobItemResource;
+use App\Http\Resources\LocationResource;
 use App\Models\JobItem;
 use App\Models\Location;
 use Illuminate\Http\Request;
@@ -11,38 +13,16 @@ class SearchController extends Controller
 {
     public function jobs(Request $request)
     {
-        $query = JobItem::query();
+        $jobs = JobItem::searchJobs($request);
 
-        // Search by job title or description
-        if ($request->filled('q')) {
-            $query->where(function ($q) use ($request) {
-                $q->where('title', 'like', '%'.$request->q.'%')
-                    ->orWhere('description', 'like', '%'.$request->q.'%');
-            });
-        }
-
-        // Pagination
-        $jobs = $query->with('company')->paginate($request->get('per_page', 5));
-
-        return response()->json($jobs);
+        return JobItemResource::collection($jobs);
     }
 
     public function locations(Request $request)
     {
-        $query = Location::query();
 
-        // Search by location state or city or country
-        if ($request->filled('q')) {
-            $query->where(function ($q) use ($request) {
-                $q->where('state', 'like', '%'.$request->q.'%')
-                    ->orWhere('city', 'like', '%'.$request->q.'%')
-                    ->orWhere('country', 'like', '%'.$request->q.'%');
-            });
-        }
+        $locations = Location::searchLocations($request);
 
-        // Pagination
-        $locations = $query->paginate($request->get('per_page', 10));
-
-        return response()->json($locations);
+        return LocationResource::collection($locations);
     }
 }
