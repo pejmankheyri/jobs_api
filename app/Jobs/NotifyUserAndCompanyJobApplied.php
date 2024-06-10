a93670430ec9e3950aa4fc2b77da1378907c4014
pejmankheyri <pejmankheyri@gmail.com>
2024-06-10 12:33:23 +0200

    remove: comments

--- app/Jobs/NotifyUserAndCompanyJobApplied.php
+++ app/Jobs/NotifyUserAndCompanyJobApplied.php
@@ -37,13 +37,9 @@ class NotifyUserAndCompanyJobApplied implements ShouldQueue
         $company = $this->jobItem->company;
 
         // Notify the user
-        // Mail::to($user->email)->send(new JobAppliedForUser($this->jobItem, $user));
-
         ThrottledMail::dispatch(new JobAppliedForUser($this->jobItem, $user), $user);
 
         // Notify the company
-        // Mail::to($company->user->email)->send(new JobAppliedForCompany($this->jobItem, $user));
-
         ThrottledMail::dispatch(new JobAppliedForCompany($this->jobItem, $user), $company->user);
 
     }
